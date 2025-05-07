d0d4cdab4ae1cb2fa74d227417b2e9292d1a4641
tkim0105 <t-kim@ssmarket.co.jp>
2025-05-07 15:41:54 +0700

    ログ出力追加

--- cloud-functions-demo/function/main.py
+++ cloud-functions-demo/function/main.py
@@ -1,16 +1,23 @@
 # pylint: disable=missing-docstring
+import logging
 import os
 
 from google.cloud import storage as gcs
+
+logging.basicConfig(level=logging.INFO)
 
 
 def hello_world(request):
     bucket_name = os.environ.get("BUCKET_NAME")
     file_name = os.environ.get("FILE_NAME", "sample.txt")
 
+    logging.info("Start reading file: %s from bucket: %s", file_name, bucket_name)
+
     client = gcs.Client()
     bucket = client.bucket(bucket_name)
     blob = bucket.blob(file_name)
     content = blob.download_as_text()
+
+    logging.info("File content: %s", content)
 
     return content
